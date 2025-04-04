3d12f696742563ad68d6b1e96385ae4ea568a25e
Diego Rojas <devup2332@gmail.com>
2025-04-04 15:43:07 -0500

    master: Edited cors conf

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { env } from './config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: env.FRONT_URL,
+      origin: '*',
       credentials: true,
     },
   });
