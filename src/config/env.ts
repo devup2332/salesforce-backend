a24eeab0e6188abcf347be782015f6dde7d2c79d
Diego Rojas <devup2332@gmail.com>
2025-04-04 15:40:00 -0500

    master: Added new env variable

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,4 +2,5 @@ export const env = {
   SUPABASE_URL: process.env.SUPABASE_URL!,
   SUPABASE_NEON_KEY: process.env.SUPABASE_NEON_KEY!,
   DATABASE_URL: process.env.DATABASE_URL!,
+  FRONT_URL: process.env.FRONT_URL!,
 };
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { env } from './config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: 'http://localhost:3002',
+      origin: env.FRONT_URL,
       credentials: true,
     },
   });
